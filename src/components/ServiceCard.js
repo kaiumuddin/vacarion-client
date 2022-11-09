ceec0c8dc76250d2b0200816044b3159006762ea
kaiumuddin <kaium.uddin2909@gmail.com>
2022-11-09 21:45:07 +0600

    service image photo view done

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -1,22 +1,32 @@
 import React from 'react';
+import {PhotoProvider, PhotoView} from 'react-photo-view';
 
 const ServiceCard = () => {
+
+    const imgUrl = "https://mdbootstrap.com/img/new/standard/nature/182.jpg";
+
     return (
-        <div className="rounded-lg shadow-lg bg-white max-w-sm">
-            <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
-                <img className="rounded-t-lg" src="https://mdbootstrap.com/img/new/standard/nature/182.jpg" alt="" />
-            </a>
-            <div className="p-6">
-                <h5 className="text-gray-900 text-xl font-medium mb-2">Name</h5>
-                <p className="text-gray-700 text-base mb-4">
-                    Price
-                </p>
-                <p className="text-gray-700 text-base mb-4">
-                    Description
-                </p>
-                <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">View Details</button>
+        <PhotoProvider>
+            <div className="rounded-lg shadow-lg bg-white max-w-sm">
+                <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                    <PhotoView src={imgUrl}>
+                        <img className="rounded-t-lg" src={imgUrl} alt="" />
+                    </PhotoView>
+                </a>
+                <div className="p-6">
+                    <h5 className="text-gray-900 text-xl font-medium mb-2">Name</h5>
+                    <p className="text-gray-700 text-base mb-4">
+                        Price
+                    </p>
+                    <p className="text-gray-700 text-base mb-4">
+                        Description
+                    </p>
+                    <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">View Details</button>
+                </div>
             </div>
-        </div>
+        </PhotoProvider>
+
+
     );
 };
 
