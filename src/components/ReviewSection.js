a1cd27e7d21b9353d5678119a1073d44a2628777
kaiumuddin <kaium.uddin2909@gmail.com>
2022-11-09 22:44:09 +0600

    Add review designed done

--- src/components/ReviewSection.js
+++ src/components/ReviewSection.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import AddReviewCard from "./AddReviewCard";
+import ReviewSectionCard from "./ReviewSectionCard";
 
 const ReviewSection = () => {
     return (
-        <div>
-            ReviewSection
-        </div>
+        <div className="my-10">
+            <h2 className="text-center text-5xl mb-10">Latest Reviews</h2>
+            <div className="grid grid-cols-3 gap-36">
+                <AddReviewCard></AddReviewCard>
+                <ReviewSectionCard></ReviewSectionCard>
+                <ReviewSectionCard></ReviewSectionCard>
+                <ReviewSectionCard></ReviewSectionCard>
+            </div>
+        </div >
     );
 };
 
-export default ReviewSection;
+export default ReviewSection;;;;;
