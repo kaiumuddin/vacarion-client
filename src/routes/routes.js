6e948159dac8635f6696fd5b7eaa9fa99e59e899
kaiumuddin <kaium.uddin2909@gmail.com>
2022-11-09 20:54:33 +0600

    Navbar modified

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,20 @@
 import {createBrowserRouter} from "react-router-dom";
 import Main from "../layout/Main";
 import ErrorPage from "../components/ErrorPage";
+import Home from "../components/Home";
 
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Main></Main>,
-        errorElement: <ErrorPage></ErrorPage>
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
     }
 ]);
 
